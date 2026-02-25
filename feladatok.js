70613fe057614a1a43885ac65c8eb5ecd7452fe9
BumbleVeee <gubek.veronika.eva@gmail.com>
2026-02-25 15:12:12 +0100

    tesztelt feladat

--- feladatok.js
+++ feladatok.js
@@ -1,5 +1,16 @@
 export function egyedi_betuk(szoveg = "") {
   // ide kell írni a megoldást ciklussal és elágazással
- 
-  return [];
+  const lista = []
+
+  for (let index = 0; index < szoveg.length; index++) {
+    let betu = szoveg[index].toLowerCase();
+    /*alakítsd át kisbetűvé a betűt*/ 
+    if (/^[a-záéíóöőúüű]$/.test(betu) && !lista.includes(betu)){
+      lista.push(betu);
+    }
+  }
+
+  lista.sort()
+  console.log(lista);
+  return lista;
 }
